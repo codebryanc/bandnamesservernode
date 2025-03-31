91b2adf03ef2e3d7edc85880d78f1174048f581e
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2025-03-31 08:09:02 -0500

    Especificar versión de Node

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,50 @@
 const { io } = require('../index');
 
-const Bands = require('../models/bandas');
-const Band = require('../models/banda');
+class Bands {
+    constructor() {
+        this.bands = [];
+    }
+
+    addBand(band = new band) {
+        this.bands.push(band);
+        return true;
+    }
+
+    getBands() {
+        return this.bands;
+    }
+
+    deleteBand(id = '') {
+        this.bands = this.bands.filter(bnd => bnd.id !== id);
+        return this.bands;
+    }
+
+    voteBand(id = '') {
+        // Recuerda que maps interactua con los registros
+        // Lo que hace es transformar la el registro que recibe por eso el else
+        this.bands = this.bands.map(band => {
+            if(band.id == id) {
+                band.votes ++;
+                return band;
+            }
+            else {
+                return band;
+            }
+        });
+    }
+}
+
+const { v4: uuidV4 } = require('uuid');
+
+class Band {
+    constructor(name = 'no-name') {
+        this.id = uuidV4(); // Identificador único
+        this.name = name;
+        this.votes = 0;
+    }
+}
+
+module.exports = Band;
 
 const bands = new Bands();
 
